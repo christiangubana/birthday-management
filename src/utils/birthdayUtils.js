a67bab51a53d42b86d5bb0d973a2795de27e8191
christiangubana <christiangubana@gmail.com>
2024-01-22 23:21:12 +0200

    updated calculateBirthdate falgorythm

--- src/utils/birthdayUtils.js
+++ src/utils/birthdayUtils.js
@@ -1,4 +1,3 @@
-// src/utils/birthdayUtils.js
 const calculateBirthdate = (idNumber) => {
   // Extracting birthdate from South African ID number (YYMMDD format)
   const year = parseInt(idNumber.substring(0, 2));
@@ -7,13 +6,41 @@ const calculateBirthdate = (idNumber) => {
 
   // Adjusting the year based on the current date
   const currentYear = new Date().getFullYear();
-  const centuryPrefix = Math.floor(currentYear / 100); // Get the century prefix
-  const fullYear = centuryPrefix * 100 + year;
+  const fullYear = year + (year < currentYear % 100 ? 2000 : 1900);
 
   const calculatedBirthdate = new Date(fullYear, month - 1, day);
+
+  // Adjust the day without adding extra day
+  calculatedBirthdate.setDate(day);
+
   console.log(`calculatedBirthdate => ${calculatedBirthdate}`);
 
   return calculatedBirthdate;
 };
 
 export { calculateBirthdate };
+
+// // src/utils/birthdayUtils.js
+// const calculateBirthdate = (idNumber) => {
+//   // Extracting birthdate from South African ID number (YYMMDD format)
+//   const year = parseInt(idNumber.substring(0, 2));
+//   const month = parseInt(idNumber.substring(2, 4));
+//   const day = parseInt(idNumber.substring(4, 6));
+
+//   // Adjusting the year based on the current date
+//   const currentYear = new Date().getFullYear();
+//   const fullYear = year + (year < currentYear % 100 ? 2000 : 1900);
+
+//   const calculatedBirthdate = new Date(fullYear, month - 1, day);
+
+//   // Adjust the day to account for the day of the week
+//   calculatedBirthdate.setDate(calculatedBirthdate.getDate() + 1);
+
+//   console.log(`calculatedBirthdate => ${calculatedBirthdate}`);
+
+//   return calculatedBirthdate;
+// };
+
+// export { calculateBirthdate };
+
+// // src/utils/birthdayUtils.js
