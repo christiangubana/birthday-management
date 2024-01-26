e93b916ca69b17731c91b2c558b4f0b893b82cf0
christiangubana <christiangubana@gmail.com>
2024-01-26 17:20:29 +0200

    remove redundat code

--- src/utils/birthdayUtils.js
+++ src/utils/birthdayUtils.js
@@ -24,28 +24,3 @@ const calculateBirthdate = (idNumber) => {
 };
 
 export { calculateBirthdate };
-
-// // src/utils/birthdayUtils.js
-// const calculateBirthdate = (idNumber) => {
-//   // Extracting birthdate from South African ID number (YYMMDD format)
-//   const year = parseInt(idNumber.substring(0, 2));
-//   const month = parseInt(idNumber.substring(2, 4));
-//   const day = parseInt(idNumber.substring(4, 6));
-
-//   // Adjusting the year based on the current date
-//   const currentYear = new Date().getFullYear();
-//   const fullYear = year + (year < currentYear % 100 ? 2000 : 1900);
-
-//   const calculatedBirthdate = new Date(fullYear, month - 1, day);
-
-//   // Adjust the day to account for the day of the week
-//   calculatedBirthdate.setDate(calculatedBirthdate.getDate() + 1);
-
-//   console.log(`calculatedBirthdate => ${calculatedBirthdate}`);
-
-//   return calculatedBirthdate;
-// };
-
-// export { calculateBirthdate };
-
-// // src/utils/birthdayUtils.js
